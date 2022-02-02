04c7b9c00feb3ca47ae9051d41bad784976a98bf
emin <50670112+emin314@users.noreply.github.com>
2022-02-02 14:21:47 +0300

    veryFirstApplication

---
+++ bullsEye/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  bullsEye
+//
+//  Created by Emin on 4.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+    
+    }
+}
+
